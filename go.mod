e733582bf61fe9982a13d5b7e98336cf7c49e24e
Marko Vidaković <markovidakovic29@gmail.com>
2023-05-31 17:14:43 +0200

    -

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module analysis
 
 go 1.19
+
+require github.com/cdipaolo/sentiment v0.0.0-20200617002423-c697f64e7f10
+
+require (
+	github.com/cdipaolo/goml v0.0.0-20220715001353-00e0c845ae1c // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/cdipaolo/sentiment"
+)
+
+func readReviews(fp string) ([]string, error) {
+	file, err := os.Open(fp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var reviews []string
+	for scanner.Scan() {
+		reviews = append(reviews, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return reviews, nil
+}
+
+func processReview(review string, results chan<- string) {
+	cleanedReview := preprocessText(review)
+	sentimentScore := analyzeSentiment(cleanedReview)
+	results <- fmt.Sprintf("Review: %s, Sentiment score: %d", review, sentimentScore)
+}
+
+func preprocessText(text string) string {
+	cleanedText := strings.ToLower(text)
+	cleanedText = strings.ReplaceAll(cleanedText, ".", "")
+	cleanedText = strings.ReplaceAll(cleanedText, ",", "")
+	// more processing steps as needed
+	return cleanedText
+}
+
+func analyzeSentiment(review string) uint8 {
+	model, err := sentiment.Restore()
+	if err != nil {
+		fmt.Println(err)
+	}
+	analysis := model.SentimentAnalysis(review, sentiment.English)
+	return analysis.Score
+}
 
 func main() {
-	fmt.Println("analysis")
+	reviews, err := readReviews("./reviews.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(chan string)
+	var wg sync.WaitGroup
+
+	for _, review := range reviews {
+		wg.Add(1)
+		go func(review string) {
+			defer wg.Done()
+			processReview(review, results)
+		}(review)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for result := range results {
+		fmt.Println(result)
+	}
 }
